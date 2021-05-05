7d4e9955f2c2bd751ec3a1867addd044c5ead9f4
Justin Clayton <jt.clayton92@yahoo.com>
2021-05-04 17:20:59 -0700

    Make bot get quotes all day for science.

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from src.data_bot import DataBot
 if __name__ == '__main__':
     load_dotenv()
     bot = DataBot()
-    bot.get_quotes(['AAPL', 'AMZN', 'FAKEDAS', 'MSFT', 'T'])
+    tracked_symbols = [
+        'AAPL', 'BNGO', 'CCL', 'COCP', 'DKNG', 'F', 'GE', 'HD', 'INTC', 'MSFT',
+        'MVIS', 'NIO', 'PLUG', 'T', 'X'
+    ]
+
+    bot.track_day(tracked_symbols)
--- src/data_bot.py
+++ src/data_bot.py
@@ -1,5 +1,7 @@
+from datetime import datetime as dt
 import os
 import requests
+import time
 
 from .util.csv_writer import CsvWriter
 from .util.logger import Logger
@@ -25,6 +27,21 @@ class DataBot:
 
             if len(row) > 0:
                 self.csv_writer.write(sym, row)
+
+    def track_day(self, symbol_list: list):
+        curr_time = dt.now().time()
+        market_open = dt.strptime('06:30', '%H:%M').time()
+        market_close = dt.strptime('13:00', '%H:%M').time()
+
+        if curr_time > market_open and curr_time < market_close:
+            print('Making requests until market close, see todays logs for details')
+            while curr_time < market_close:
+                self.get_quotes(symbol_list)
+                time.wait(0.05)
+            print('Market has closed')
+        else:
+            print('Market is not open')
+
 
     def __extract_data(self, sym: str, data: dict):
         '''Extract desired data from response'''
